54221480b57c2fea5e8bae1ececb689c365205ba
BerugrIngiO <bergurol342@nemi.tskoli.is>
2018-01-18 14:00:28 +0000

    verkefni1

---
+++ Verkefni-1.py
@@ -0,0 +1,18 @@
+from bottle import route, run
+
+@route("/")
+def index():
+    return "<a href=/about> About</a>" "<a> </a>" "<a href=/bio> Biography</a>" "<a> </a>" "<a href=/pictures> Pictures</a>"
+@route("/about")
+def about():
+    return "Hér eru upplýsngar um Steve Jobs"
+
+@route("/bio")
+def bio():
+    return "Hér er biography frá Steve Jobs"
+
+@route("/pictures")
+def pictures():
+    return "Hérna eru myndir frá lífi Steve Jobs"
+
+run()
